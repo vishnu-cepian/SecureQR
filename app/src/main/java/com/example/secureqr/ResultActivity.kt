2dee68d4a8a651ab1c1a00eadab53f3a0ec136f8
vishnu <vishnurpillai2003@gmail.com>
2024-12-17 21:58:48 +0530

    Displayed the Hashed Content for development purpose

--- app/src/main/java/com/example/secureqr/ResultActivity.kt
+++ app/src/main/java/com/example/secureqr/ResultActivity.kt
@@ -1,7 +1,6 @@
 package com.example.secureqr
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
