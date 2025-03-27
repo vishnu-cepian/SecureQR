fa7df9dc4910d1f202fe191c212591493e74423d
vishnu <vishnurpillai2003@gmail.com>
2025-03-27 20:22:03 +0530

    Created New API call function for Fetching Company names from Database

--- app/src/main/java/com/example/secureqr/RetrofitClient.kt
+++ app/src/main/java/com/example/secureqr/RetrofitClient.kt
@@ -4,8 +4,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val DL_BASE_URL = "http://192.168.37.96:5000"  // Replace with your API URL
+    private const val DL_BASE_URL = "http://192.168.37.96:5000"
     private const val IPFS_BASE_URL = "https://gateway.pinata.cloud/"
+    private const val DB_BASE_URL = "http://192.168.183.96:5000/api/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
@@ -22,4 +23,13 @@ object RetrofitClient {
             .build()
         retrofit.create(IpfsApi::class.java)
     }
+
+    val mongoApi: RetrofitInterface by lazy {
+        Retrofit.Builder()
+            .baseUrl(DB_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RetrofitInterface::class.java)
+    }
+
 }
--- app/src/main/java/com/example/secureqr/RetrofitInterface.kt
+++ app/src/main/java/com/example/secureqr/RetrofitInterface.kt
@@ -2,14 +2,22 @@ package com.example.secureqr
 
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.Headers
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 data class UrlRequest(val url: String)
 data class ApiResponse(val prediction: String)
+data class Company( val _id: String,
+                    val name: String
+)
 
 interface ApiService {
-//    @Headers("Content-Type: application/json")
-    @POST("/predict")  // Change this to your actual Flask API endpoint
+    @POST("/predict")
     fun getPrediction(@Body request: UrlRequest): Call<ApiResponse>
+
+}
+
+interface RetrofitInterface {
+    @GET("companies")
+    fun getCompanies(): Call<List<Company>>
 }
